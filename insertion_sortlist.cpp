8d96e3e3be3483bde82cf189f7c1f1006f9949dd
Ji.Zhilong <zhilongji@gmail.com>
2014-02-23 14:13:41 +0800

    try insertion sort on a list.

---
+++ insertion_sortlist.cpp
@@ -0,0 +1,89 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  insertion_sortlist.cpp
+ *
+ *    Description:  Sort a linked list using insertion sort.
+ *
+ *        Version:  1.0
+ *        Created:  02/22/2014 14:10:21 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Ji.Zhilong (), zhilongji@gmail.com
+ *   Organization:  SJTU
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *fromArray(int *x, int len) {
+  if (x == NULL || len <= 0)
+    return NULL;
+
+  ListNode tmp(0);
+  ListNode *tail = &tmp;
+  for (int i = 0; i < len; i++) {
+    tail->next = new ListNode(x[i]);
+    tail = tail->next;
+  }
+  return tmp.next;
+}
+
+class Solution {
+  public:
+    ListNode *sortList(ListNode *head) {
+      int len = listLength(head);
+      int n, i, l1, l2;
+      if (len <= 1)
+        return head;
+      ListNode tmp(0);
+      tmp.next = head;
+      for (int i = 1; i < len; i++) {
+
+      }
+
+    }
+
+    int listLength(ListNode *head) {
+      int res = 0;
+      for (; head; head = head->next)
+        res++;
+      return res;
+    }
+
+    void printList(ListNode *head) {
+      for (; head; head = head->next)
+        cout << head->val << endl;
+      cout << endl;
+    }
+
+};
+
+
+int
+main(int argc, char *argv[])
+{
+  int arr1[] = {4, 19, 14, 5, -3, 1, 8, 5, 11, 15};
+  int arr2[] = {1, 2, 3, 4, 5};
+  ListNode *head1 = fromArray(arr1, 10);
+  ListNode *head2 = fromArray(arr2, 5);
+
+  Solution solution;
+  head1 = solution.sortList(head1);
+  solution.printList(head1);
+  return 0;
+}
+
+
