1144e458cf1658ee69d0a8499e0fcf0275e45dbb
Ji.Zhilong <zhilongji@gmail.com>
2014-02-25 16:38:15 +0800

    solution 1(wrong) for candy problem

--- candy.cpp
+++ candy.cpp
@@ -24,13 +24,42 @@
  */
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
   public:
     int candy(vector<int> &ratings) {
-      return 0;
+      vector<vector<int> > updown(2);
+      int downing = 1;
+      int total = 0;
+      int len = ratings.size();
+
+      for (int i = 0; i < len;) {
+        int j, rt = ratings[i];
+        for (j = i+1; j < len && ratings[j] == rt; j++ ); /* count continueing same ratings */
+        updown[downing].push_back(j - i);
+        i = j;
+
+        if (j == len || (downing == 1 && ratings[j] > rt)) { /* get to a minimum point or reach the last rating  */
+          downing = 0;
+          int su = updown[0].size(), sd = updown[1].size();
+
+          for (int m = 0; m < su - 1; m++)
+            total += (m+2)*updown[0][m];
+          for (int n = 0; n < sd; n++)
+            total += (sd - n)*updown[1][n];
+
+          if (su > 0)
+            total += updown[0][su - 1]*max(1+su, sd);   /* how many candy should the kid with maximum rating get */
+          updown[0].clear(); updown[1].clear();
+        } else if (downing == 0 && ratings[j] < rt){ /* get to a maximum point */
+          downing = 1;
+        }
+      }
+
+      return total;
     }
 };
 
@@ -38,7 +67,7 @@ class Solution {
 int
 main()
 {
-  int arr[] = {1, 2, 1, 4, 3, 5, 2, 7, 8, 1};
+  int arr[] = {};
   vector<int> ratings(arr, arr + sizeof(arr)/sizeof(int));
   Solution solution;
   cout << solution.candy(ratings) << endl;
