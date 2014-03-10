bd3533752e26ee1e1ec4bf62b608f6f7a33c772d
Ji.Zhilong <zhilongji@gmail.com>
2014-03-10 12:13:47 +0800

    a correct but MLE solution

--- word_ladder2.cpp
+++ word_ladder2.cpp
@@ -26,47 +26,44 @@ using namespace std;
 
 class Solution {
   public:
-    vector<vector<string> > findLadders(string start, string end, unordered_set<string> &dict) {
-      dict.insert(end);
-      unordered_map<string, vector<string> > map;
+    vector<vector<string> > findLadders(string start, string end, unordered_set<string> &dic) {
+      vector<string> dict(dic.begin(), dic.end());
+      dict.push_back(end);
+      dict.insert(dict.begin(), start);
+      unordered_map<string, vector<int> > map;
+      int starti = 0, endi = dict.size() - 1;
 
-      for (auto it = dict.begin(); it != dict.end(); it++) {
-          string tmp = *it;
-          for (int i = 0; i < tmp.length(); i++) {
-            tmp[i] = '\0';
+      for (int i = 1; i < dict.size(); i++) {  /* record the variations of every word in dict */
+          string tmp = dict[i];
+          for (int j = 0; j < tmp.length(); j++) {
+            tmp[j] = '\0';
             if (map.find(tmp) == map.end())
-              map[tmp] = vector<string>();
-            map[tmp].push_back(*it);
-            tmp[i] = (*it)[i];
+              map[tmp] = vector<int>();
+            map[tmp].push_back(i);
+            tmp[j] = dict[i][j];
           }
       }
 
-      unordered_set<string> set;
-      queue<string> q;
-      queue<vector<string> > trans;
-      vector<vector<string> > res;
-      int min = -1;
+      queue<int> q;
+      queue<vector<int> > trans;
+      vector<vector<int> > res;
 
-      set.insert(start);
-      q.push(start);
-      trans.push(vector<string>());
+      q.push(starti);
+      trans.push(vector<int>());
 
       while (!q.empty()) {
-        string tmp = q.front(); q.pop();
-        vector<string> tran = trans.front(); trans.pop();
+        int tmpi = q.front(); q.pop();
+        string tmp = dict[tmpi];
+        vector<int> tran = trans.front(); trans.pop();
 
-        tran.push_back(tmp);
+        tran.push_back(tmpi);
 
         if (!res.empty() && tran.size() > res[0].size())
           break;
 
-        if (tmp == end) {
-          if (res.empty() || tran.size() == res[0].size()) {
-            res.push_back(tran);
-            continue;
-          } else {
-            break;
-          }
+        if (tmpi == endi) {
+          res.push_back(tran);
+          continue;
         }
 
         for (int i = 0; i < tmp.length(); i++) {
@@ -78,14 +75,24 @@ class Solution {
           }
           for (auto it = map[tmp].begin(); it != map[tmp].end(); it++) {
             q.push(*it);
-            set.insert(*it);
             trans.push(tran);
           }
           tmp[i] = tc;
         }
       }
 
-      return res;
+      vector<vector<string> > result;
+      if (res.empty())
+        return result;
+
+      for (int i = 0; i < res.size(); i++) {
+        result.push_back(vector<string>());
+        for (int j = 0; j < res[i].size(); j++) {
+          result.back().push_back(dict[res[i][j]]);
+        }
+      }
+
+      return result;
     }
 };
 
@@ -103,6 +110,13 @@ main(int argc, char *argv[])
   dict.insert("log");
 
   Solution solution;
-  cout << solution.findLadders(start, end, dict).size() << endl;
+  vector<vector<string> > res = solution.findLadders(start, end, dict);
+
+  for (int i = 0; i < res.size(); i++) {
+    for (int j = 0; j < res[i].size(); j++) {
+      cout << res[i][j] << " ";
+    }
+    cout << endl;
+  }
 }
 
