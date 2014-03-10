116bc062676111404dd6464fdc00a4bcbe63de50
Ji.Zhilong <zhilongji@gmail.com>
2014-03-10 12:58:13 +0800

    another MLE version

--- word_ladder2.cpp
+++ word_ladder2.cpp
@@ -18,77 +18,60 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <utility>
 #include <unordered_set>
-#include <unordered_map>
 #include <iostream>
 
 using namespace std;
+typedef pair<int, char> t;
 
 class Solution {
   public:
-    vector<vector<string> > findLadders(string start, string end, unordered_set<string> &dic) {
-      vector<string> dict(dic.begin(), dic.end());
-      dict.push_back(end);
-      dict.insert(dict.begin(), start);
-      unordered_map<string, vector<int> > map;
-      int starti = 0, endi = dict.size() - 1;
-
-      for (int i = 1; i < dict.size(); i++) {  /* record the variations of every word in dict */
-          string tmp = dict[i];
-          for (int j = 0; j < tmp.length(); j++) {
-            tmp[j] = '\0';
-            if (map.find(tmp) == map.end())
-              map[tmp] = vector<int>();
-            map[tmp].push_back(i);
-            tmp[j] = dict[i][j];
-          }
-      }
-
-      queue<int> q;
-      queue<vector<int> > trans;
-      vector<vector<int> > res;
-
-      q.push(starti);
-      trans.push(vector<int>());
+    vector<vector<string> > findLadders(string start, string end, unordered_set<string> &dict) {
+      queue<string> q;
+      queue<vector<t> > trans;
+      vector<vector<t> > res;
+      trans.push(vector<t>());
+      q.push(start);
 
       while (!q.empty()) {
-        int tmpi = q.front(); q.pop();
-        string tmp = dict[tmpi];
-        vector<int> tran = trans.front(); trans.pop();
+        string tmp = q.front(); q.pop();
+        vector<t> tran = trans.front(); trans.pop();
 
-        tran.push_back(tmpi);
-
-        if (!res.empty() && tran.size() > res[0].size())
+        if (!res.empty() && tran.size() >= res[0].size())
           break;
-
-        if (tmpi == endi) {
-          res.push_back(tran);
-          continue;
-        }
 
         for (int i = 0; i < tmp.length(); i++) {
           char tc = tmp[i];
-          tmp[i] = '\0';
-          if (map.find(tmp) == map.end()) {
-            tmp[i] = tc;
-            continue;
+          for (char c = 'a'; c <= 'z'; c++) {
+            if (c != tc) {
+              tmp[i] = c;
+              if (tmp == end) {
+                res.push_back(tran);
+                res.back().push_back(t(i, c));
+                goto endloop;
+              }
+              if (dict.find(tmp) != dict.end()) {
+                q.push(tmp);
+                trans.push(tran);
+                trans.back().push_back(t(i, c));
+              }
+              tmp[i] = tc;
+            }
           }
-          for (auto it = map[tmp].begin(); it != map[tmp].end(); it++) {
-            q.push(*it);
-            trans.push(tran);
-          }
-          tmp[i] = tc;
         }
+endloop:;
       }
 
       vector<vector<string> > result;
-      if (res.empty())
-        return result;
 
       for (int i = 0; i < res.size(); i++) {
         result.push_back(vector<string>());
+        result.back().push_back(start);
         for (int j = 0; j < res[i].size(); j++) {
-          result.back().push_back(dict[res[i][j]]);
+          string tmp = result.back().back();
+          tmp[res[i][j].first] = res[i][j].second;
+          result.back().push_back(tmp);
         }
       }
 
