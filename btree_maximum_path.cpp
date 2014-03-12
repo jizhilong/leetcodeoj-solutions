1a4bca6739d5db733144cbf98877d183623f1cd2
Ji.Zhilong <zhilongji@gmail.com>
2014-03-12 20:02:37 +0800

    add a iterative solution for binary tree maximum path sum problem.

--- btree_maximum_path.cpp
+++ btree_maximum_path.cpp
@@ -24,6 +24,7 @@
  */
 
 #include <iostream>
+#include <stack>
 #include <algorithm>
 
 using namespace std;
@@ -37,36 +38,53 @@ struct TreeNode {
 
 struct Result {
   int res;  /* the actual result with the current node as root */
+  int resn;
   int resl; /* the maximum path without any right child's node */
   int resr; /* the maximum path without any left child's node */
 
-  Result(int n1, int n2, int n3):res(n1), resl(n2), resr(n3){};
+  Result(Result *l, Result *r, TreeNode *n) {
+      resr = resl = n->val;
+      if (l)
+        resl += l->resn;
+      if (r)
+        resr += r->resn;
+      
+      resn = max(0, max(resl, resr));
+      res = resl + resr - n->val;
+      if (l && l->res > res)
+        res = l->res;
+      if (r && r->res > res)
+        res = r->res;
+  }
 };
 
 class Solution {
-  private:
-    Result _maxPathSum(TreeNode *node) {
-      if (node == NULL)
-        return Result(0,0,0);
-
-      Result l = _maxPathSum(node->left);
-      Result r = _maxPathSum(node->right);
-      int resl = node->val + max(0, max(l.resl, l.resr));
-      int resr = node->val + max(0, max(r.resl, r.resr));
-      int res = resl + resr - node->val;
-
-      if (node->left && l.res > res)
-        res = l.res;
-      if (node->right && r.res > res)
-        res = r.res;
-
-      return Result(res, resl, resr);
-    }
-
   public:
     int maxPathSum(TreeNode *root) {
-      Result res = _maxPathSum(root);
-      return root ? res.res : 0;
+      stack<TreeNode *> s;
+      stack<Result *> st;
+
+      for (TreeNode *tmp = root; tmp; tmp = tmp->left) {
+        s.push(tmp); s.push(tmp);
+      }
+
+      st.push(NULL);
+
+      while (!s.empty()) {
+        TreeNode *tmp = s.top(); s.pop();
+        if (s.empty() || tmp != s.top()) {
+          Result *r = st.top(); st.pop();
+          Result *l = st.top(); st.pop();
+          st.push(new Result(l, r, tmp));
+      } else {
+        for (TreeNode *node = tmp->right; node; node= node->left) {
+          s.push(node);s.push(node);
+        }
+        st.push(NULL); 
+      }
+    }
+
+      return st.top()->res;
     }
 };
 
@@ -86,8 +104,10 @@ main(int argc, char *argv[]) {
   TreeNode *root2 = new TreeNode(1);
   root2->left = new TreeNode(-2);
   root2->right = new TreeNode(3);
+  root2->right->left = new TreeNode(1);
+  root2->right->right = new TreeNode(2);
   
   Solution solution;
   cout << solution.maxPathSum(root) << endl;
-  cout << solution.maxPathSum(root2) << endl;
+//  cout << solution.maxPathSum(root2) << endl;
 }
