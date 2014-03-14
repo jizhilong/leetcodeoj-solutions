b526a65bc098f4253807f1acdbbf0b1d3918eba4
Ji.Zhilong <zhilongji@gmail.com>
2014-03-14 14:30:34 +0800

    add solution for distinct subsequences.

--- distinct_subsequences.cpp
+++ distinct_subsequences.cpp
@@ -23,14 +23,31 @@
  */
 
 #include <string>
+#include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
   public:
     int numDistinct(string S, string T) {
+      vector<vector<int> > dp(2, vector<int>(T.size(), 0));
 
+      for (int i = 0; i < S.size(); i++) {
+        char c = S[i];
+        int prev = i % 2;
+        int cur = 1 - prev;
+        for (int j = 0; j < T.size(); j++) {
+          if (c == T[j]) {
+            dp[cur][j] = dp[prev][j] + (j > 0 ? dp[prev][j-1] : 1);
+          } else {
+            dp[cur][j] = dp[prev][j];
+          }
+        }
+      }
+
+      return max(dp[0].back(), dp[1].back());
     }
 };
 
