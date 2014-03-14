a5d3cb8df8482b05a14e046ce01ca3c10e5b4bff
Ji.Zhilong <zhilongji@gmail.com>
2014-03-14 16:00:30 +0800

    add a solution for path sum.

---
+++ path_sum.cpp
@@ -0,0 +1,75 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  path_sum.cpp
+ *
+ *    Description:  Given a binary tree and a sum, determin if the tree has a 
+ *                  root to leaf path such that adding up all the values along
+ *                  the path equals the given sum.
+ *
+ *        Version:  1.0
+ *        Created:  03/14/2014 03:44:32 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Ji.Zhilong (), zhilongji@gmail.com
+ *   Organization:  SJTU
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {};
+};
+
+class Solution {
+  public:
+    bool hasPathSum(TreeNode *root, int sum) {
+      queue<TreeNode *> q;
+      queue<int> acc;
+      if (root) {
+        q.push(root);
+        acc.push(root->val);
+      }
+
+      while (!q.empty()) {
+        TreeNode *node = q.front(); q.pop();
+        int cur = acc.front(); acc.pop();
+        if (cur == sum && node->left == NULL && node->right == NULL)
+          return true;
+        if (node->left) {
+          q.push(node->left);
+          acc.push(node->left->val + cur);
+        }
+        if (node->right) {
+          q.push(node->right);
+          acc.push(node->right->val + cur);
+        }
+      }
+
+      return false;
+    }
+};
+
+int
+main(int argc, char *argv[]) {
+  TreeNode *root = new TreeNode(5);
+  root->left = new TreeNode(4);
+  root->right = new TreeNode(8);
+  root->left->left = new TreeNode(11);
+  root->left->left->left = new TreeNode(7);
+  root->left->left->right = new TreeNode(2);
+  root->right->left  = new TreeNode(13);
+  root->right->right = new TreeNode(4);
+  root->right->right->right = new TreeNode(1);
+
+  Solution solution;
+  cout << solution.hasPathSum(root, 22) << endl;
+}
