c0645d48e751d6fdb62ffd3e8a500ff198f622f0
Ji.Zhilong <zhilongji@gmail.com>
2014-03-21 14:40:42 +0800

    get ready for rotating image.

---
+++ rotate_image.cpp
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  rotate_image.cpp
+ *
+ *    Description:  You are given an n x n 2D matrix repersenting an image.
+ *                  Rotate the image by 90 degrees(clockwise).
+ *
+ *        Version:  1.0
+ *        Created:  03/21/2014 02:26:49 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Ji.Zhilong (), zhilongji@gmail.com
+ *   Organization:  SJTU
+ *
+ * =====================================================================================
+ */
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+  public:
+    void rotate(vector<vector<int> > &matrix) {
+
+    }
+};
+
+int
+main() {
+
+}
