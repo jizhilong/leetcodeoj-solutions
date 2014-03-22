dcf07621755b0c6382543eedb7542689943b278c
Ji.Zhilong <zhilongji@gmail.com>
2014-03-22 21:10:46 +0800

    a constant extra space solution for rotating image.

--- rotate_image.cpp
+++ rotate_image.cpp
@@ -27,9 +27,14 @@ class Solution {
     void rotate(vector<vector<int> > &matrix) {
       vector<vector<int> > back = matrix;
       int n = matrix.size();
-      for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-          matrix[j][n-1-i] = back[i][j];
+      for (int i = n/2 - 1; i >= 0; i--) {
+        for (int j = (n+1)/2 - 1; j >= 0; j--) {
+          int tmp;
+          tmp = matrix[n-1-j][i];
+          matrix[n-1-j][i] = matrix[n-1-i][n-1-j]; /* update the bottom element */
+          matrix[n-1-i][n-1-j] = matrix[j][n-1-i]; /* update the right bottom element */
+          matrix[j][n-1-i] = matrix[i][j];    /* update the right element */
+          matrix[i][j] = tmp;                 /* update the current element */
         }
       }
     }
@@ -37,13 +42,14 @@ class Solution {
 
 int
 main() {
-  vector<vector<int> > matrix = {{1,2}, {1, 2}};
+  vector<vector<int> > matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
   Solution solution;
   for (int i = 0; i < matrix.size(); i++) {
     for (int j = 0; j < matrix[i].size(); j++)
       cout << matrix[i][j] << " ";
     cout << endl;
   }
+  cout << endl;
   solution.rotate(matrix);
   for (int i = 0; i < matrix.size(); i++) {
     for (int j = 0; j < matrix[i].size(); j++)
