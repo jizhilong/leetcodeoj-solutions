7c3e242fcf99fc595735d667c069f9a4e6629e47
Ji.Zhilong <zhilongji@gmail.com>
2014-03-22 18:08:02 +0800

    a quite simple solution for rotating image.

--- rotate_image.cpp
+++ rotate_image.cpp
@@ -25,11 +25,29 @@ using namespace std;
 class Solution {
   public:
     void rotate(vector<vector<int> > &matrix) {
-
+      vector<vector<int> > back = matrix;
+      int n = matrix.size();
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          matrix[j][n-1-i] = back[i][j];
+        }
+      }
     }
 };
 
 int
 main() {
-
+  vector<vector<int> > matrix = {{1,2}, {1, 2}};
+  Solution solution;
+  for (int i = 0; i < matrix.size(); i++) {
+    for (int j = 0; j < matrix[i].size(); j++)
+      cout << matrix[i][j] << " ";
+    cout << endl;
+  }
+  solution.rotate(matrix);
+  for (int i = 0; i < matrix.size(); i++) {
+    for (int j = 0; j < matrix[i].size(); j++)
+      cout << matrix[i][j] << " ";
+    cout << endl;
+  }
 }
