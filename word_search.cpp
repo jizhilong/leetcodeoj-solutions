38ca0bbd98bc1774fd2538c92eefb8d90739d07d
Ji.Zhilong <zhilongji@gmail.com>
2014-04-04 19:27:15 +0800

    an AC solution for word search.

--- word_search.cpp
+++ word_search.cpp
@@ -35,63 +35,51 @@
 
 using namespace std;
 
+#define update(condition1) if (condition1 && board[i][j] == word[len] && !mark[i][j]) {\
+                                mark[i][j] = true; \
+                                if (exist(board, i, j, len+1)) \
+                                    return true; \
+                                mark[i][j] = false;}
+
 class Solution {
+  private:
+    string word;
+    vector<vector<bool> > mark;
+    bool exist(vector<string> &board, int i, int j, int len) {
+      if (len == word.length())
+        return true;
+
+      j--;
+      update(j>=0);
+      j += 2;
+      update(j<board[0].size());
+      j--;
+      i--;
+      update(i>=0);
+      i+=2;
+      update(i<board.size());
+
+      return false;
+    }
+
   public:
     bool exist(vector<string> &board, string word) {
-      vector<vector<bool> > mark(board.size(), vector<bool>(board[0].length(), false));
-      queue<int> is;
-      queue<int> js;
-      queue<int> lens;
-      queue<vector<vector<bool> > > marks;
+      if (word.size() == 0)
+        return true;
+      if (board.empty())
+        return false;
+
+      mark = vector<vector<bool> >(board.size(), vector<bool>(board[0].length(), false));
+      this->word = word;
 
       for (int i = 0; i < board.size(); i++) {
         for (int j = 0; j < board[i].length(); j++) {
           if (board[i][j] == word[0]) {
-            is.push(i);
-            js.push(j);
-            lens.push(1);
-            vector<vector<bool> > tmpmark = mark;
-            tmpmark[i][j] = true;
-            marks.push(tmpmark);
+            mark[i][j] = true;
+            if (exist(board, i, j, 1))
+              return true;
+            mark[i][j] = false;
           }
-        }
-      }
-
-      while (!is.empty()) {
-        int i = is.front(); is.pop();
-        int j = js.front(); js.pop();
-        int len = lens.front(); lens.pop();
-        vector<vector<bool> > tmpmark = marks.front(); marks.pop();
-
-        if (len == word.length())
-          return true;
-
-        if (j-1>=0 && board[i][j-1] == word[len] && !tmpmark[i][j-1]) {
-          is.push(i); js.push(j-1);lens.push(len+1);
-          vector<vector<bool> > tmp = tmpmark;
-          tmp[i][j-1] = true;
-          marks.push(tmp);
-        }
-
-        if (j+1<board[0].size() && board[i][j+1] == word[len] && !tmpmark[i][j+1]) {
-          is.push(i); js.push(j+1);lens.push(len+1);
-          vector<vector<bool> > tmp = tmpmark;
-          tmp[i][j+1] = true;
-          marks.push(tmp);
-        }
-
-        if (i-1>=0 && board[i-1][j] == word[len] && !tmpmark[i-1][j]) {
-          is.push(i-1); js.push(j);lens.push(len+1);
-          vector<vector<bool> > tmp = tmpmark;
-          tmp[i-1][j] = true;
-          marks.push(tmp);
-        }
-
-        if (i+1<board.size() && board[i+1][j] == word[len] && !tmpmark[i+1][j]) {
-          is.push(i+1); js.push(j);lens.push(len+1);
-          vector<vector<bool> > tmp = tmpmark;
-          tmp[i+1][j] = true;
-          marks.push(tmp);
         }
       }
 
@@ -102,10 +90,8 @@ class Solution {
 int
 main()
 {
-  vector<string> board = {"ABCE", "SFCS", "ADEE"};
+  vector<string> board = {"ABCE","SFCS", "ADEE"};
   Solution solution;
   cout << solution.exist(board, "ABCCED") << endl;
-  cout << solution.exist(board, "SEE") << endl;
-  cout << solution.exist(board, "ABCB") << endl;
 }
 
