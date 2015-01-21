f2ce032d447a891958251fd6e5f182c475d524b2
Zhilong.JI <zlji@Ctrip.com>
2015-01-21 22:45:53 +0800

    majority number.

---
+++ majority_element.cpp
@@ -0,0 +1,58 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  majority_element.cpp
+ *
+ *    Description:  Given an array of size n, find the majority element. The majority
+ *                  element is the element that appears more then [n/2] times.
+ *                  You may assume that the array is non empty and the majority element
+ *                  always exist in the array.
+ *
+ *        Version:  1.0
+ *        Created:  01/21/2015 10:27:41 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Ji.Zhilong (), zhilongji@gmail.com
+ *   Organization:  SJTU
+ *
+ * =====================================================================================
+ */
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+  public:
+    int majorityElement(vector<int> &num) {
+      sort(num.begin(), num.end());
+      int lim = num.size() / 2;
+      int count = 1, last = num[0];
+
+      for (int i = 1; i < num.size(); i++) {
+        if (num[i] == last) {
+          count++;
+        } else {
+          if (count >= lim) {
+            return last;
+          } else {
+            count = 1;
+            last = num[i];
+          }
+        }
+      }
+     return last;
+    }
+};
+
+
+int
+main(int argc, char *argv[]) {
+  Solution s;
+  vector<int> data;
+  data.push_back(1);
+  cout << s.majority_element(data) << endl;
+}
