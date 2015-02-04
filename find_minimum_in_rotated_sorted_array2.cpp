aaee354d16746cc879abd97ddd27838dd70b7ec0
Ji.Zhilong <zlji@Ctrip.com>
2015-02-04 20:41:30 +0800

    Finding minimum in a rotated sorted array,
    which may have duplicated elements.

---
+++ find_minimum_in_rotated_sorted_array2.cpp
@@ -0,0 +1,60 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  find_minimum_in_rotated_sorted_array.cpp
+ *
+ *    Description:  Suppose a sorted array is rotated at some pivot unknown to
+ *                  you beforehand.
+ *                  (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2)
+ *                  Find the minimum element.
+ *                  You can not assume no duplicate exists in the array.
+ *
+ *        Version:  1.0
+ *        Created:  01/30/2015 12:25:52 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Ji.Zhilong (), zhilongji@gmail.com
+ *   Organization:  SJTU
+ *
+ * =====================================================================================
+ */
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+  public:
+    int findMin(vector<int> &num){
+      int i = 0, j = num.size() - 1;
+      while (i < j) {
+        if (num[i] < num[j])
+          return num[i];
+        int m = i + ((j - i) >> 1);
+        if (num[m] > num[i]) {
+          i = m + 1;
+        } else if (num[m] < num[i]){
+          j = m;
+        } else {
+          int res = num[i];
+          for (int tmp = i+1; tmp <= j; tmp++) {
+            res = num[tmp] < res ? num[tmp]: res;
+          }
+          return res;
+        }
+      }
+      return num[i];
+    }
+};
+
+int
+main(int argc, char *argv[])
+{
+  int anums[] = {10, 1, 10, 10, 10};
+  vector<int> vnums = vector<int>(anums, anums+sizeof(anums)/sizeof(int));
+
+  Solution s;
+  cout << s.findMin(vnums) << endl;
+}
